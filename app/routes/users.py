e7e296e78e33ed54c3a4500c2c489b70ab3b4175
safiqq <szarifin20041@gmail.com>
2023-12-08 23:40:03 +0700

    chore: asmain nama var param

--- app/routes/users.py
+++ app/routes/users.py
@@ -46,12 +46,12 @@ async def generate_token(form_data: OAuth2PasswordRequestForm = Depends()):
 
 
 @user_router.post("/register")
-async def register(user_data: User):
+async def register(form_data: User):
     """
     Registers a new user.
 
     Args:
-        user_data: A User object containing user information.
+        form_data: A User object containing user information.
 
     Returns:
         A dictionary containing a success message.
@@ -59,11 +59,12 @@ async def register(user_data: User):
     Raises:
         HTTPException 400: If a user with the same username already exists.
     """
-    user = db.get_user_by_username(user_data.username)
+    user = db.get_user_by_username(form_data.username)
     if user:
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST, detail="User already exists"
         )
-    user_data.password = pwd_context.hash(user_data.password)
-    _id = db.create_user(user_data.to_db())
+    form_data.password = pwd_context.hash(form_data.password)
+    _id = db.create_user(form_data.to_db())
+    # print(form_data)
     return {"message": f"User created successfully with ID {_id}"}
